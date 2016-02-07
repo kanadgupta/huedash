87b1a7e2583e81ad8249a318f57629d3602cb058
Kanad Gupta <kgupta@umn.edu>
2016-02-07 10:49:33 -0600

    Added log handler, function for one lightbulb switch

--- dashtest.py
+++ dashtest.py
@@ -5,6 +5,8 @@ from scapy.all import *
 from beautifulhue.api import Bridge
 import datetime
 import time
+import csv
+import sys
 
 
 username = 'beautifulhuetest'
@@ -18,17 +20,19 @@ def arp_display(pkt):
 
         if pkt[ARP].hwsrc == 'f0:27:2d:ed:68:24': # Gatorade
           print "Gatorade button pressed - Timestamp: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-          dashLightSwitch()
+          logHandler("Gatorade button pressed")
+          oneDashLightSwitch()
 
     else:
       print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+      logHandler("ARP Probe from unknown device: " + pkt[ARP].hwsrc)
 
 def dashLightSwitch():
     resource = {'which':0}
     data = bridge.group.get(resource)
     isOn = data['resource']['action']['on']
     action = {
-                 'which':1,
+                 'which':0,
                  'data':{
                      'action':{
                          'on':not isOn
@@ -38,8 +42,47 @@ def dashLightSwitch():
     switchedTo = "off" if isOn else "on"
     bridge.group.update(action)
     print "Lights turned " + switchedTo + " - Timestamp: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    logHandler("Lights turned " + switchedTo)
+
+def oneDashLightSwitch():
+    resource = {'which':2}
+    data = bridge.light.get(resource)
+    isOn = data['resource']['state']['on']
+    state = {
+                 'which':2,
+                 'data':{
+                     'state':{
+                         'on':not isOn
+                     }
+                 }
+             }
+    switchedTo = "off" if isOn else "on"
+    bridge.light.update(state)
+    print "One light turned " + switchedTo + " - Timestamp: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    logHandler("One light turned " + switchedTo)
+
+def recursiveSniffer():
+    try:
+        print sniff(prn=arp_display, filter="arp", store=0)
+    except KeyboardInterrupt:
+        print "Bye!!"
+        logHandler("Keyboard end")
+        return
+    print "Starting again: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    logHandler("Recursive sniff")
+    recursiveSniffer()
+
+def logHandler(inputString='default'):
+    with open('log.csv', 'a') as csvlog:
+        a = csv.writer(csvlog, delimiter=',')
+        timeLog = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        logRow = [inputString, timeLog]
+        a.writerow(logRow)
+
 
 print "Beginning sniffing"
-print sniff(prn=arp_display, filter="arp", store=0)
+logHandler("Beginning sniffing")
+recursiveSniffer()
 print "Fin"
+logHandler("Sniff complete")
 
