470bd2fbaeefe72c16964399748940fe0314a276
Kanad Gupta <kgupta@umn.edu>
2016-03-26 17:12:02 -0500

    Added two light switch method

--- dashtest.py
+++ dashtest.py
@@ -12,7 +12,7 @@ username = 'beautifulhuetest'
 bridge = Bridge(device={'ip':'10.1.10.18'}, user={'name':username})
 
 def arp_display(pkt):
-    
+
     if pkt[ARP].op == 1: #who-has (request)
 
       if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
@@ -23,8 +23,7 @@ def arp_display(pkt):
           oneDashLightSwitch()
 
     else:
-      # print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
-      logHandler("ARP Probe from unknown device: " + pkt[ARP].hwsrc)
+      print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
 def groupDashLightSwitch(num=0):
     resource = {'which':num}
@@ -60,16 +59,48 @@ def oneDashLightSwitch(num=2):
     print "One light turned " + switchedTo + " - Timestamp: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     logHandler("Light " + str(num) + " turned " + switchedTo)
 
+def twoDashLightSwitch(num1=1, num2=2):
+    resource1 = {'which':num1}
+    data1 = bridge.light.get(resource1)
+    isOn1 = data1['resource']['state']['on']
+    resource2 = {'which':num2}
+    data2 = bridge.light.get(resource2)
+    isOn2 = data2['resource']['state']['on']
+
+    if (isOn1 or isOn2) == True:
+        setLightsTo = False
+    else:
+        setLightsTo = True
+    state1 = {
+                 'which':num1,
+                 'data':{
+                     'state':{
+                         'on': setLightsTo
+                     }
+                 }
+             }
+    state2 = {
+                 'which':num2,
+                 'data':{
+                     'state':{
+                         'on': setLightsTo
+                     }
+                 }
+             }
+    switchedTo = "on" if setLightsTo else "off"
+    bridge.light.update(state1)
+    bridge.light.update(state2)
+    print "Two lights turned " + switchedTo + " - Timestamp: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    logHandler("Light " + str(num1) + " and " + str(num2) + " turned " + switchedTo)
+
 def recursiveSniffer():
-    try:
-        print sniff(prn=arp_display, filter="arp", store=0)
-    except KeyboardInterrupt:
-        print "Bye!!"
-        logHandler("Keyboard end")
-        return
+    print sniff(prn=arp_display, filter="arp", store=0)
     print "Starting again: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     logHandler("Recursive sniff")
     recursiveSniffer()
+
+def sniffer():
+    print sniff(prn=arp_display, filter="arp", store=0)
 
 def logHandler(inputString='default'):
     with open('log.csv', 'a') as csvlog:
@@ -79,9 +110,8 @@ def logHandler(inputString='default'):
         a.writerow(logRow)
 
 
-print "Beginning sniffing with PID " + str(os.getpid()) 
+print "Beginning sniffing with PID " + str(os.getpid())
 logHandler("Beginning sniffing with PID " + str(os.getpid()))
-recursiveSniffer()
+sniffer()
 print "Fin"
 logHandler("Sniff complete")
-
